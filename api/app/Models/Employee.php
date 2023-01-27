599364a70aec3143ad81cf7a7af25594acc15fbc
Ethan Moss <ethan.moss@sykescottages.co.uk>
2023-01-27 17:26:13 +0000

    Create Employee Model

---
+++ api/app/Models/Employee.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    use HasFactory;
+
+    // Fields which can be assigned by an incoming request
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email_address',
+        'mobile_number',
+        'pin'
+    ];
+
+    // Fields which cannot be assigned
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at'
+    ];
+
+    // Provides the data type for each field
+    protected $casts = [
+        'first_name' => 'string',
+        'last_name' => 'string',
+        'email_address' => 'string',
+        'mobile_number' => 'sting',
+        'pin' => 'string'
+    ];
+}
