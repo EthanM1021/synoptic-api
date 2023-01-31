76b9498f4f0f0e55d4ed247ea3e7861e1bb971a7
Ethan Moss <ethan.moss@sykescottages.co.uk>
2023-01-31 15:16:04 +0000

    Complete model

--- api/app/Models/Card.php
+++ api/app/Models/Card.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Card extends Model
 {
     use HasFactory;
+
+    protected $table = 'cards';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'credit'
+    ];
+
+    protected $guarded = [
+        'id',
+        '_fk_employee_id',
+        'created_at',
+        'updated_at'
+    ];
+
+    protected $casts = [
+        'credit' => 'float'
+    ];
 }
